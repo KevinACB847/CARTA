96673cba6acd1fa18ff7682ce11998b37abd1987
KevinACB847 <kevincastellon416@gmail.com>
2025-09-28 18:16:09 -0400

    Update intro.js

--- JS/intro.js
+++ JS/intro.js
@@ -30,7 +30,7 @@ const SETTINGS = {
   T_RUSH_START_MS   : 40000,
   T_WARP_START_MS   : 60000,  // 58.8s (Canvas warp ~1.2s)
   T_PREBOOM_MS      : 60000,  // 59.5s (tensión previa)
-  T_BOOM_MS         : 61000,  // 62.0s (retrasado)
+  T_BOOM_MS         : 60500,  // 62.0s (retrasado)
 
   // Beats sincronizados alrededor del título (54–59s)
   T_STAR_BEATS_START_MS : 54000, // inicio del patrón
